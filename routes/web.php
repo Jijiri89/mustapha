55625e22fb2e91a52046de5ed2352e338898e8e2
Jijiri89 <jijirijohnson@gmail.com>
2023-11-23 16:34:40 +0000

    homepage

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,23 @@ use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/',\App\Livewire\Home::class)->name('home');
 
-Route::get('/', function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Route::get('/', function () {
     $blog=Blog::all();
     return view('welcome',compact('blog'));
 })->name('home');
@@ -30,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';*/
