afc4393c129b0598ec3d7aeff372efdf51fd9735
Jijiri89 <jijirijohnson@gmail.com>
2023-11-24 15:30:47 +0000

    modi

--- resources/views/livewire/nav.blade.php
+++ resources/views/livewire/nav.blade.php
@@ -7,7 +7,7 @@
             </a>
 
             <!-- Mobile Menu Button (Hamburger Icon) -->
-            <button id="mobile-menu-button" class="lg:hidden text-white hover:text-green-500 focus:outline-none">
+            <button id="mobile-menu-button" class=" mx-2 lg:hidden text-white hover:text-green-500 focus:outline-none">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                 </svg>
