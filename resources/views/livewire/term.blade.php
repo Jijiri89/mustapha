5dd50ace38499c17f415378e988571ef391e1bf9
Jijiri89 <jijirijohnson@gmail.com>
2023-11-25 04:54:55 +0000

    mod

--- resources/views/livewire/term.blade.php
+++ resources/views/livewire/term.blade.php
@@ -4,24 +4,24 @@
             <div class="w-full card card border-yellow-900 bg-white shadow-md p-8">
                 <h2 class="text-2xl font-semibold text-yellow-800 mb-4">Terms and Conditions</h2>
 
-                <p class="whitespace-normal leading-normal"><strong>WhatsApp:</strong> 0245631300</p>
-                <p class="whitespace-normal leading-normal"><strong>Telephone:</strong> 0207787847 / 0245631300</p>
-                <p class="whitespace-normal leading-normal"><strong>Email:</strong> seidumustaphastoph@gmail.com</p>
+                <p class="whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>WhatsApp:</strong> 0245631300</p>
+                <p class="whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Telephone:</strong> 0207787847 / 0245631300</p>
+                <p class="whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Email:</strong> seidumustaphastoph@gmail.com</p>
 
-                <p class="mt-4 whitespace-normal leading-normal"><strong>Service Overview:</strong>
+                <p class="mt-4 whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Service Overview:</strong>
                     JAZAKH-ALLAH VENTURES provides innovative solutions and services to meet your needs.</p>
 
-                <p class="mt-4 whitespace-normal leading-normal"><strong>Mobile Menu:</strong>
+                <p class="mt-4 whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Mobile Menu:</strong>
                     Our mobile menu is accessible by clicking the "Hamburger Icon" (three horizontal lines) on the top-right corner of our website. It provides quick access to Home, About, Services, and Contact links.</p>
 
                 <!-- Add more terms and conditions as needed -->
 
-                <p class="mt-4 whitespace-normal leading-normal"><strong>Contacting Us:</strong>
+                <p class="mt-4 whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Contacting Us:</strong>
                     For inquiries or assistance, feel free to reach out to us through WhatsApp, telephone, or email.</p>
 
                 <!-- Add more sections as needed -->
 
-                <p class="mt-4 whitespace-normal leading-normal"><strong>Acceptance of Terms:</strong>
+                <p class="mt-4 whitespace-normal leading-normal sm:whitespace-pre-line sm:leading-pre-line md:whitespace-pre-line md:leading-pre-line"><strong>Acceptance of Terms:</strong>
                     By using our services, you agree to abide by these terms and conditions.</p>
 
                 <p class="mt-8">Thank you for choosing JAZAKH-ALLAH VENTURES. We look forward to serving you.</p>
