928c5c96a292add3c8e3e17a2137bfe65461df45
Jijiri89 <jijirijohnson@gmail.com>
2023-11-25 05:12:13 +0000

    mod

--- resources/views/livewire/term.blade.php
+++ resources/views/livewire/term.blade.php
@@ -1,17 +1,23 @@
 <div class="container card border border-yellow-900 lg:mx-1 m-1 p-8">
     <div class="w-full card card  border-yellow-900 bg-white shadow-md p-8">
-        <h1 class="text-2xl font-bold text-yellow-900 text-center  mb-4">Welcome to JAZAKH-ALLAH VENTURES</h1>
+        <h1 class="text-2xl font-bold text-yellow-900 text-center mb-4">Terms and Conditions</h1>
 
         <p class="text-lg text-blue-800 leading-loose">
-            JAZAKH-ALLAH VENTURES is your trusted partner for all your construction needs. We specialize in the sales and hiring of top-quality machines and a wide range of building materials. Whether you are planning a small DIY project or undertaking a large-scale construction endeavor, we have the tools and materials you need to bring your vision to life.
+            Welcome to JAZAKH-ALLAH VENTURES, your trusted partner for all your needs. By using our services, you agree to abide by the following terms and conditions.
         </p>
 
         <p class="text-lg text-blue-800 leading-loose mt-4">
-            Our commitment to excellence, reliability, and customer satisfaction sets us apart in the industry. Explore our extensive inventory of construction machinery and high-quality building materials to ensure the success of your projects. At JAZAKH-ALLAH VENTURES, we are dedicated to providing you with the best solutions for your construction requirements.
+            JAZAKH-ALLAH VENTURES is dedicated to providing innovative solutions and services to meet your needs. Our commitment to excellence, reliability, and customer satisfaction sets us apart in the industry.
         </p>
 
         <p class="text-lg text-blue-800 leading-loose mt-4">
-            Contact us today to discuss your project needs, and let us be your partner in success. Trust JAZAKH-ALLAH VENTURES for all your sales and hiring needs in the construction and building materials industry.
+            Please take the time to review the terms outlined below. If you have any questions or concerns, feel free to contact us. Your use of our services constitutes acceptance of these terms and conditions.
+        </p>
+
+        <!-- Add more terms and conditions as needed -->
+
+        <p class="text-lg text-blue-800 leading-loose mt-4">
+            Thank you for choosing JAZAKH-ALLAH VENTURES. We look forward to serving you and providing the best solutions for your requirements.
         </p>
     </div>
 </div>
